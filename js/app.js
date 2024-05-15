c7083e9e550f209cb5bb9c5b018ff6b17663946a
minStory <142480973+minStory@users.noreply.github.com>
2024-05-16 01:47:15 +0900

    latte site upload

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const parentElement = document.querySelector(".header-menu ul");
+
+//   const childElements = parentElement.children;
+
+//   for (let i = childElements.length - 1; i > 0; i--) {
+//     const span = document.createElement("span");
+//     span.style.borderLeft = "1px solid black";
+//     span.style.height = "14px";
+//     span.style.margin = "0 10px";
+
+//     parentElement.insertBefore(span, childElements[i]);
+//   }
+// });
