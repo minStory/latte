e5e3aaae955e8f7d689d889311888eff29b60422
minStory <142480973+minStory@users.noreply.github.com>
2024-05-16 17:10:19 +0900

    rev1

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const parentElement = document.querySelector(".header-menu ul");
-
-//   const childElements = parentElement.children;
-
-//   for (let i = childElements.length - 1; i > 0; i--) {
-//     const span = document.createElement("span");
-//     span.style.borderLeft = "1px solid black";
-//     span.style.height = "14px";
-//     span.style.margin = "0 10px";
-
-//     parentElement.insertBefore(span, childElements[i]);
-//   }
-// });
+// 페이지 로드 시 실행되는 코드
+document.addEventListener("DOMContentLoaded", () => {
+	// 스크롤 관련 메서드
+    $(window).scroll(function(){ 
+		const scroll = $("html").scrollTop();
+		const header_container = document.getElementById("header-container");
+		const header_bottom = document.getElementById("header-bottom");
+			
+		// 스크롤을 내렸을 때 헤더가 변경
+		if(scroll > 60){
+			$("#header-top").fadeOut(0);
+			header_container.classList.add("header-container-invert");
+			header_bottom.classList.add("header-bottom-invert");
+		// 스크롤을 다시 올렸을 때 헤더 원상복귀
+		}else{
+			$("#header-top").fadeIn(0);
+			header_container.classList.remove("header-container-invert");
+			header_bottom.classList.remove("header-bottom-invert");
+		}
+	})
+})
